0bb9d67a03dee3c23c54d72795860b88de71ccc0
Elton Guilherme <elton.guilherme@hotmail.com>
2026-03-06 00:38:57 -0300

    Inicialização da API

--- ProjetoCriacaodeAPIcomMongoDB/index.js
+++ ProjetoCriacaodeAPIcomMongoDB/index.js
@@ -6,12 +6,19 @@ const app = express();
 app.use(
     express.urlencoded({ 
         extended: true 
-    }), //inicia leitura de dados do body
+    }), //inicia a configuração de leitura de JSON via urlencoded
 )
 
-app.use(express.json());    //inicia leitura de JSON
+app.use(express.json());    //Vai poder ler JSON via body
 
 
 // rota inicial / endpoint
+app.get('/', (req, res) => {     //Criando uma rota GET para o endpoint raiz
+
+    //mostrar req
+    res.json({ message: 'Oi Express!' })   //Resposta em JSON com uma mensagem
+})
+
 
 //entregar uma porta
+app.listen(3000)       //Inicia o servidor/API na porta 3000
